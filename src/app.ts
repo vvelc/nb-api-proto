7184aa0a47b346831afd98a3b56a854e4f7318a0
Victor Velazquez Cid <76626605+vvelc@users.noreply.github.com>
2024-09-23 15:43:10 -0400

    Change message

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+  res.send('Hello, World! from V1');
 });
 
 app.listen(port, () => {
