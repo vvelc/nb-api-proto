88d7057cd3b7d7695463368a68bde081d85047f7
Victor Velazquez Cid <76626605+vvelc@users.noreply.github.com>
2024-09-24 21:39:47 -0400

    Add v2 comment on startup message and add logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 const port = process.env.PORT ?? 3000;
@@ -7,6 +7,11 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World! from V2');
 });
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`${Date.now()} - ${req.method} ${req.url}}`)
+  next();
+})
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`V2 Server running on port ${port}`);
 });
